927a07ded6011add5a3644ad312abef25525856c
Hamzah Aslam <59409878+HamzahCR7@users.noreply.github.com>
2024-03-04 17:00:37 +0530

    Update Contact-Details.tsx
    Added countries dropdown which will filter states based on that

--- vite-project/src/Contact-Details.tsx
+++ vite-project/src/Contact-Details.tsx
@@ -23,11 +23,10 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod"
 import { formSchema } from "./ContactSchema";
-import './Contact-Details.css'
+import './Contact-details.css'
+import countriess from '../src/assets/countries.json';
 
 function ContactDetails() {
-  const [selectedOption, setSelectedOption] = useState('');
-  const [validationResult, setValidationResult] = useState({ success: true, error: null });
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -36,110 +35,175 @@ function ContactDetails() {
         },
       });
       function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
         console.log(values)
       }
-
     
-      // Function to handle the change in dropdown value
+      const [selectedCountry, setSelectedCountry] = useState(Number);
+    
+      const handleCountryChange = (value: any) => {
+        setSelectedCountry(value);
+        
+      };
+    
+  
+      const selectedCountryObject = countriess.find((country: any) =>
+      { return country.country === selectedCountry
+      });
+      const statesForSelectedCountry = selectedCountryObject ? selectedCountryObject['states'] : [];
+
     
   return (
     <>
-    <h3>Contact-Details</h3>
-    <div className="form-container">
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="username" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>            
-          )}
-        />
-            <FormField
-          control={form.control}
-          name="useremail"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Useremail</FormLabel>
-              <FormControl>
-                <Input placeholder="useremail" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>                    
-          )}
-        />
-          <FormField
-          control={form.control}
-          name="gender"
-          render={({ field }) => (
-            <FormItem className="space-y-3">
-              <FormLabel>Gender</FormLabel>
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="flex flex-col space-y-1"
-                >
-                  <FormItem className="flex items-center space-x-3 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="male" />
-                    </FormControl>
-                    <FormLabel className="font-normal">
-                      Male
-                    </FormLabel>
+      <h3>Contact-Details</h3>
+      <div className="form-container">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="form-content">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="row">
+                      <div className="col-3">
+                        <FormLabel>Username</FormLabel>
+                      </div>
+                      <div className="col-9">
+                        <FormControl className="col-9">
+                          <Input placeholder="username" {...field} />
+                        </FormControl>
+                      </div>
+                    </div>
+                    <FormMessage />
                   </FormItem>
-                  <FormItem className="flex items-center space-x-3 space-y-0">
+                )}
+              />
+            </div>
+            <div className="form-content">
+              <FormField
+                control={form.control}
+                name="useremail"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Useremail</FormLabel>
                     <FormControl>
-                      <RadioGroupItem value="female" />
+                      <Input placeholder="useremail" {...field} />
                     </FormControl>
-                    <FormLabel className="font-normal">
-                      Female
-                    </FormLabel>
+                    <FormMessage />
                   </FormItem>
-                </RadioGroup>
-              </FormControl>
-              <FormMessage />
-              </FormItem>
-          )}
-          />
-       <FormField
-          control={form.control}
-          name="country"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a Country of your residence" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="India">India</SelectItem>
-                  <SelectItem value="Aus">Australia</SelectItem>
-                  <SelectItem value="PR">Portugal</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="mt-2 submit">Submit</Button>
-      </form>
-    </Form>
-    </div>
- 
+                )}
+              />
+            </div>
+            <div className="form-content">
+              <FormField
+                control={form.control}
+                name="gender"
+                render={({ field }) => (
+                  <FormItem className="space-y-3 form-item">
+                    <FormLabel>Gender</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        className="flex flex-col space-y-1"
+                      >
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="male" />
+                          </FormControl>
+                          <FormLabel className="font-normal">Male</FormLabel>
+                        </FormItem>
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="female" />
+                          </FormControl>
+                          <FormLabel className="font-normal">Female</FormLabel>
+                        </FormItem>
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="form-content">
+              <FormField
+                control={form.control}
+                name="country"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Country</FormLabel>
+                    <Select
+                      onValueChange={(value: any) => {
+                        field.onChange(value);
+                        handleCountryChange(value);
+                      }}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a Country" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {countriess.map((country: any) => (
+                          <SelectItem
+                            key={country.countryId}
+                            value={country.country}
+                          >
+                            {country.country}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
+            <div className="form-content">
+              <FormField
+                control={form.control}
+                name="state"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>State</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a State" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {statesForSelectedCountry.map((state: any) => (
+                          <SelectItem
+                            key={state.stateId}
+                            value={state.state}
+                            style={{ color: "red" }}
+                          >
+                            {state.state}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <Button type="submit" className="mt-2 submit">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </div>
     </>
-  )
+  );
           
 }
 
